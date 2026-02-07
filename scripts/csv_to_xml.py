c918a331045f2fb3b20db57ac07540ea70098d81
cominixo01 <lucasdutraluza@protonmail.com>
2026-02-07 11:49:37 +0100

    Add weblate csv to xml conversion script

---
+++ scripts/csv_to_xml.py
@@ -0,0 +1,62 @@
+import csv
+import lxml.etree
+import lxml.builder
+
+csvfile = open("outer-wilds-game-tok.csv", newline='')
+gamereader = csv.reader(csvfile)
+next(gamereader, None)
+
+E = lxml.builder.ElementMaker()
+root = E.TranslationTable_XML
+entry = E.entry
+key = E.key
+value = E.value
+shiplog_entry = E.TranslationTableEntry
+ui_entry = E.TranslationTableEntryUI
+table_shipLog = E.table_shipLog
+table_ui = E.table_ui
+
+the_doc = root()
+
+for row in gamereader:
+    the_doc.append(
+        entry (
+            key(row[5]),
+            value(row[2])
+        )
+    )
+
+csvfile = open("outer-wilds-ships-logs-tok.csv", newline='')
+shiplogs = csv.reader(csvfile)
+next(shiplogs, None)
+
+table = table_shipLog()
+
+for row in shiplogs:
+    table.append(
+        shiplog_entry (
+            key(row[5]),
+            value(row[2])
+        )
+    )
+
+the_doc.append(table)
+
+csvfile = open("outer-wilds-ui-tok.csv", newline='')
+ui = csv.reader(csvfile)
+next(ui, None)
+
+table = table_ui()
+
+for row in ui:
+    table.append(
+        ui_entry (
+            key(row[5]),
+            value(row[2])
+        )
+    )
+
+the_doc.append(table)
+
+et = lxml.etree.ElementTree(the_doc)
+et.write('Translation.xml', pretty_print=True)
